4c6c493778c0067d0916ae4fd53233e5d634bb4b
kiejah <kiejah@gmail.com>
2024-08-23 10:47:29 +0300

    added secret key environ var

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,10 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-5u8=wf0l+h%r^rtz75_s&-6_be=3!48p20p=be1jwor4#1&07a"
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower()=="true"
+# DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower()=="true"
+DEBUG = config("DJANGO_DEBUG",cast=bool)
+
+print("DEBUG",DEBUG,type(DEBUG))
 
 ALLOWED_HOSTS = [
     ".railway.app"
